9f5c20d7e5b1303a0813ae5601f7fd6659374d09
manishvbn <manishvbn@gmail.com>
2022-06-22 11:00:04 +0530

    5-Using-State-With-Class-Component

---
+++ Day1/1_basic-understanding/5-Using-State-With-Class-Component/index.js
@@ -0,0 +1,36 @@
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().toLocaleString()
+        }
+    }
+
+    tick() {
+        this.setState(() => {
+            return ({
+                time: new Date().toLocaleString()
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return React.createElement('h1',
+            { className: 'orange' },
+            'Hello World from React ' + this.state.time
+        );
+    }
+}
+
+ReactDOM.render(
+    React.createElement(Hello, {}, null),
+    document.getElementById('app')
+);
