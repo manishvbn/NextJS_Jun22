62f13f5107b43ff463740c3d859e1a8f8918e20d
manishvbn <manishvbn@gmail.com>
2022-06-22 11:26:47 +0530

    7-Building-an-App-with-Nextjs-and-JSX

---
+++ Day1/1_basic-understanding/7-Building-an-App-with-Nextjs-and-JSX/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().toLocaleString()
+        }
+    }
+
+    tick() {
+        this.setState(() => {
+            return ({
+                time: new Date().toLocaleString()
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <h1 className="orange">
+                Hello World From React {this.state.time}
+            </h1>
+        );
+    }
+}
+
+export default Hello;
