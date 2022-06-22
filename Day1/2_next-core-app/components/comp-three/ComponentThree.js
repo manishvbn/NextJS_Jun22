3b261fb2e693ba3f19fa7678ba3160fa76969d57
manishvbn <manishvbn@gmail.com>
2022-06-22 23:31:44 +0530

    Update ComponentThree.js

--- Day1/2_next-core-app/components/comp-three/ComponentThree.js
+++ Day1/2_next-core-app/components/comp-three/ComponentThree.js
@@ -3,16 +3,17 @@ import React from 'react';
 const ComponentThree = () => {
     return (
         <div className='para'>
-            <style jsx>
-                {
-                    `
-                        .para {
-                            font-size: 20px;
-                            background-color: 'yellow-green'
-                        }
-                    `
+            <style jsx>{`
+                .para {
+                    font-size: 20px;
+                    background-color: yellowgreen;
                 }
-            </style>
+            `}</style>
+            <style jsx global>{`
+                body {
+                    background: lightgray;
+                }
+            `}</style>
             <p>Hello from Component Three</p>
         </div>
     );
